710f83c9f084a15b0a4ed5f4b92e14d90ec71b35
Vasa Hari kumar <134670368+Harikumar7733@users.noreply.github.com>
2024-11-30 20:56:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import Information from "./information/Information";
+import Header from "./header/Header";
+
+import "./index.css"
+
+function App(){
+  return(
+    <div>
+      <Header/>
+      <Information/>
+    </div>
+  )
+}
+export default App;
